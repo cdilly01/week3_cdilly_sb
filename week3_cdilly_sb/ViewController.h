db11b5a54811cfe7c5979fae3271dac9636551cf
Robert Holzhauser <Robert@Wisdomstrategy.com>
2015-06-09 01:21:55 -0600

    ...

--- week3_cdilly_sb/ViewController.h
+++ week3_cdilly_sb/ViewController.h
@@ -9,7 +9,11 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITextField *noteInputText;
 
+- (IBAction)addNote:(id)sender;
+- (IBAction)deleteNote:(id)sender;
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @end
 
--- week3_cdilly_sb/ViewController.m
+++ week3_cdilly_sb/ViewController.m
@@ -7,12 +7,16 @@
 //
 
 #import "ViewController.h"
+#import "Note.h"
+#import "NoteSvcCache.h"
 
 @interface ViewController ()
 
 @end
 
 @implementation ViewController
+
+NoteSvcCache *noteSvcCache = nil;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -24,4 +28,53 @@
     // Dispose of any resources that can be recreated.
 }
 
+// Save Note
+- (IBAction)addNote:(id)sender {
+    [self.view endEditing:YES];
+    
+    // get array of notes
+    NSMutableArray *notes = [noteSvcCache retrieveAllNotes];
+    
+    // instantiate new note with sender note
+    Note *note = [[Note alloc] init];
+    note.noteText = _noteInputText.text;
+    
+    // find index...
+    NSUInteger index = [notes indexOfObject: note ];
+    
+    // add note if doesn't exist
+    if (index == NSNotFound){
+        NSLog(@"saveNote: adding new note");
+        [noteSvcCache createNote:note];
+    }
+    else{
+        // if note exists, delete then add
+        [notes removeObjectAtIndex: index];
+        NSLog(@"deleteNote: note deleted");
+        
+        // now add...
+        NSLog(@"saveNote: adding new note");
+    }
+    
+    [self.tableView reloadData];
+    NSLog(@"saveNote: note saved");
+}
+
+- (IBAction)deleteNote:(id)sender {
+    NSLog(@"deleteNote");
+    
+    // get array of notes
+    NSMutableArray *notes = [noteSvcCache retrieveAllNotes];
+    
+    // instantiate new note with sender note
+    Note *note = [[Note alloc] init];
+    note.noteText = _noteInputText.text;
+    NSUInteger index = [notes indexOfObject: note ];
+    
+    if (index != NSNotFound){
+        [notes removeObjectAtIndex: index];
+        [self.tableView reloadData];
+        NSLog(@"deleteNote: note deleted");
+    }
+}
 @end
