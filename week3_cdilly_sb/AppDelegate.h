6c27883e382f89b97053efb78502b4e982927bf9
Robert Holzhauser <Robert@Wisdomstrategy.com>
2015-06-09 00:47:30 -0600

    Initial Commit

---
+++ week3_cdilly_sb/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  week3_cdilly_sb
+//
+//  Created by A Student on 6/9/15.
+//  Copyright (c) 2015 msse650. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
